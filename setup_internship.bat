bcfaa908c362ae41c529817c828fa3502276c25a
Arijit Kulkarni <153367667+xpw1337@users.noreply.github.com>
2026-02-24 15:38:59 -0500

    initial commit

---
+++ setup_internship.bat
@@ -0,0 +1,16 @@
+@echo off
+
+:: Open two new Firefox windows
+start firefox -new-window
+start firefox -new-window
+
+:: Open the folder in File Explorer
+explorer "D:\Studies\JHU"
+
+:: Open the PDF file (will use your default PDF reader)
+start "" "D:\Studies\JHU\Arijit_Kulkarni_resume.pdf"
+
+:: Open the text file (will use your default text editor)
+start "" "D:\Studies\JHU\internshipStuff.txt"
+
+exit
